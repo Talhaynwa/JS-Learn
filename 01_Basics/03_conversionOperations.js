795ffb2a9b74caba136ed4975d4f981eea3926ed
Talha Sayed <115915396+Talhaynwa@users.noreply.github.com>
2023-11-01 18:01:01 +0000

    conversions of datattypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score)  //Here Number will convert string value to number value
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // as score ="33abc" it will give NaN value. It also an special type of datatypes
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 false => 0
+
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // it will give true
+
+
+// 1=> true 0=> false
+// " => false"
+// "Talha" => true
+
+
+let someNumber = 33
+let stringnumber = String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
