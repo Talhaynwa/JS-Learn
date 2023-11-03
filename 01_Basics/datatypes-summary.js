01aa620ebafb1507c5d573ed0e751587e72db956
Talha Sayed <115915396+Talhaynwa@users.noreply.github.com>
2023-11-03 00:09:25 +0000

    datattype_summary

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -8,30 +8,47 @@
 // 7. Symbol => to make element unique
 // 8. BigInt
 
-const score = 100;
-const valvue = 100.3;
+const score = '100';
+// console.log(typeof score); => String
+
+const value = 100.3;
+// console.log(typeof value); => Number
 
 const isLoggedIn = false
+// console.log(isLoggedIn); => Boolean
+
 const outsideTemp = null
+// console.log(outsideTemp); => null
+
 let userEmail;
+// console.log(userEmail); => undefined
 
 const id = Symbol('123')
-const anotherId = Symbol('123')
+// console.log(id); => Symbol
 
-console.log(id === anotherId);
+const anotherId = Symbol('123')
+// console.log(id === anotherId); => false
 
 const bigNumber = 3466643165168431n
+// console.log(bigNumber); => BigInt
 
 
 /*Reference datatypes (Non Primitive): */
 //Array, Objects, Functions
+// Non Primitive datatype always return as Type of object.
 
 const heros = ["shaktiman", "naagraj", "doga"]
+// console.log(heros); => Array
+
+
 let myObj = {
     name: "talha",
     age: 28,
 }
+// console.log(myObj); => Object
 
-const myfunction(){
+
+const myfunction = function(){
     console.log("Hello World");
 }
+// myfunction(); => Object Function
